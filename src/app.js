3da7b8923d7d8fac5208ac8604ca701599371306
HC18 <harshit.chugh18@gmail.com>
2025-10-28 06:25:49 +0530

    refactor: update route imports and declarations in app.js; change JWT middleware import in comment.route.js

--- src/app.js
+++ src/app.js
@@ -20,10 +20,25 @@ app.use(express.static('public'));
 app.use(cookieParser()); //there are options in this also if needed 
 
 //routes import
-import userRoutes from './routes/user.routes.js';
+import userRouter from './routes/user.routes.js';
+import healthcheckRouter from './routes/healthcheck.route.js';
+import tweetRouter from './routes/tweet.route.js';
+import subscriptionRouter from './routes/subscription.route.js';
+import videoRouter from './routes/video.route.js';
+import likeRouter from './routes/like.route.js';
+import playlistRouter from './routes/playlist.route.js';
+import commentRouter from './routes/comment.route.js';
+import dashboardRouter from './routes/dashboard.route.js';
 //routes declaration
-app.use("/api/v1/users", userRoutes);    //http://localhost:8000/api/v1/users/<anyroute in user.routes.js>
-
+app.use("/api/v1/users", userRouter);    //http://localhost:8000/api/v1/users/<anyroute in user.routes.js>
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 app.use(errorHandler);
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // require('dotenv').config();
 import dotenv from "dotenv";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
