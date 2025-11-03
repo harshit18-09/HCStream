1bcd9cf20e85eb0f5ffe8104ebaaed21d917c82f
HC18 <harshit.chugh18@gmail.com>
2025-11-04 02:46:32 +0530

    Fix refresh token route by removing JWT verification for improved accessibility

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -34,7 +34,7 @@ router.route("/login").post(LoginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(verifyJWT, refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
